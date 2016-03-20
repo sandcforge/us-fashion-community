8729f45936b8b70bff63fb754167481fe072e6bd
CHEN LIAO <chenliao@CHENs-MacBook-Air.local>
2016-03-19 22:24:14 -0700

    About page adds Navbar.

--- app/js/pages/About.js
+++ app/js/pages/About.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {
   Container,
   Group,
+  NavBar,
 } from 'amazeui-touch';
 
 const About = React.createClass({
   render: function() {
     return (
       <Container {...this.props}>
+      <NavBar
+        title='关于'
+        amStyle="primary"
+      />
         <Group>
           <h2>2016 SandCForge @ 版权所有</h2>
         </Group>
